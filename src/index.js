d795b4377fcfa91e47b47e203f02cbe7b14fb1c3
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-08-22 20:38:19 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from "./App";
 const div = document.querySelector('#root');
 const root = ReactDOM.createRoot(div);
